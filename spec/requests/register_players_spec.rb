150ecdb1af22e90147c28020e57f1ed57ed06a96
Jamie Wright <jamie@brilliantfantastic.com>
2018-02-09 00:39:56 -0500

    Only respond to json requests

--- spec/requests/register_players_spec.rb
+++ spec/requests/register_players_spec.rb
@@ -1,41 +1,41 @@
 require "rails_helper"
 
 RSpec.describe "POST /api/players" do
+  let(:avatar) { "THIS NEEDS TO BE A BASE64 STRING" }
   let(:json_response) { JSON.parse(response.body).deep_symbolize_keys }
-
-  context "with a valid request" do
-    let(:avatar) { "THIS NEEDS TO BE A BASE64 STRING" }
-    let(:parameters) do
-      {
-        data: {
-          type: "player",
-          attributes: {
-            first_name: "Jimmy",
-            last_name: "Page",
-            email_address: "jimmy@example.com",
-            password: "p@ssword",
-            avatar: avatar
-          }
+  let(:valid_parameters) do
+    {
+      data: {
+        type: "player",
+        attributes: {
+          first_name: "Jimmy",
+          last_name: "Page",
+          email_address: "jimmy@example.com",
+          password: "p@ssword",
+          avatar: avatar
         }
       }
-    end
+    }
+  end
+
+  context "with a valid request" do
     let(:player) { Player.unscoped.last }
 
     it "returns the correct status" do
-      post "/api/players", params: parameters
+      post "/api/players", params: valid_parameters
 
-      expect(response).to have_http_status(:created)
+      expect(response).to be_created
     end
 
     it "creates a player" do
-      expect { post "/api/players", params: parameters }.to \
+      expect { post "/api/players", params: valid_parameters }.to \
         change { Player.count }.by 1
 
       expect(player.email_address).to eq "jimmy@example.com"
     end
 
     it "returns the json representation of a player" do
-      post "/api/players", params: parameters
+      post "/api/players", params: valid_parameters
 
       expect(json_response[:data][:type]).to eq "player"
       expect(json_response[:data][:id]).to eq player.id.to_s
@@ -88,6 +88,11 @@ RSpec.describe "POST /api/players" do
   end
 
   context "without a correct json content type" do
-    xit "returns a not found status"
+    it "returns a bad request status" do
+      post "/api/players", params: valid_parameters,
+                           headers: { "Content-type": "application/xml" }
+
+      expect(response).to be_bad_request
+    end
   end
 end
