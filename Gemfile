b45f28c7aabf45aca8b4240c2f0a5393aacd414c
Jamie Wright <jamie@brilliantfantastic.com>
2018-02-08 23:56:06 -0500

    Player serializer to serialize a player model into a json api object

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem "puma", "~> 3.7"
 gem "rack-cors"
 
 gem "bcrypt"
+gem "fast_jsonapi"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
---
+++ spec/serializers/player_serializer_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe PlayerSerializer do
+  let(:player) { create :player }
+
+  subject { described_class.new(player) }
+
+  describe "#serializable_hash" do
+    let(:hash) { subject.serializable_hash[:data] }
+
+    it "serializes the type" do
+      expect(hash[:type]).to eq :player
+    end
+
+    it "serializes the id" do
+      expect(hash[:id]).to eq player.id.to_s
+    end
+
+    it "serializes the attributes" do
+      expect(hash[:attributes].keys).to \
+        match_array [:avatar, :email_address, :first_name, :last_name]
+      expect(hash[:attributes][:email_address]).to eq player.email_address
+    end
+  end
+end
