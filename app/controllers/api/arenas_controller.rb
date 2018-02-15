38d8295627cc116685d64e5b519ea140aa24f990
Jamie Wright <jamie@brilliantfantastic.com>
2018-02-14 21:09:48 -0500

    Initial route and controller for arenas

---
+++ app/controllers/api/arenas_controller.rb
@@ -0,0 +1,5 @@
+class API::ArenasController < ApplicationController
+  def index
+    head :ok
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
+    resources :arenas, only: :index
     resources :players, only: :create
   end
 end
