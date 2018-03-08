d2091bba22572a4f5b3c17b5fd2b0b8188d64a18
Jamie Wright <jamie@brilliantfantastic.com>
2018-03-08 18:50:37 -0500

    Add matches check for an encrypted password

--- lib/password_encryptor.rb
+++ lib/password_encryptor.rb
@@ -4,4 +4,11 @@ class PasswordEncryptor
   def self.encrypt(password, salt)
     ::BCrypt::Password.create([password, salt].join, cost: 10)
   end
+
+  def self.matches?(password, crypted_password, salt)
+    return false if crypted_password.nil?
+    hash = ::BCrypt::Password.new(crypted_password)
+    return false if hash.nil? || hash.empty?
+    hash == [password, salt].join
+  end
 end
--- spec/lib/password_encryptor_spec.rb
+++ spec/lib/password_encryptor_spec.rb
@@ -19,4 +19,34 @@ RSpec.describe PasswordEncryptor do
       expect(result1).to_not eq result2
     end
   end
+
+  describe ".matches?" do
+    let(:password) { "hello world" }
+    let(:salt) { "123456788" }
+    let(:crypted_password) { described_class.encrypt password, salt }
+
+    it "returns true if password matches encrypted password" do
+      result = described_class.matches? password, crypted_password, salt
+
+      expect(result).to be_truthy
+    end
+
+    it "returns false if password doesn't match encrypted password" do
+      result = described_class.matches? "blah", crypted_password, salt
+
+      expect(result).to be_falsey
+    end
+
+    it "returns false if password is nil" do
+      result = described_class.matches? nil, crypted_password, salt
+
+      expect(result).to be_falsey
+    end
+
+    it "returns encrypted password  if password is nil" do
+      result = described_class.matches? password, nil, salt
+
+      expect(result).to be_falsey
+    end
+  end
 end
