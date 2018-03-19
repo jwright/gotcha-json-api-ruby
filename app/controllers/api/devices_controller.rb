6d4b828610adf7d0c2600139e0787351e4297d33
Jamie Wright <jamie@brilliantfantastic.com>
2018-03-19 19:16:59 -0400

    Create the device for the player

--- app/controllers/api/devices_controller.rb
+++ app/controllers/api/devices_controller.rb
@@ -1,5 +1,16 @@
 class API::DevicesController < ApplicationController
   def create
+    Device.create! device_params.merge(player_id: current_user.id,
+                                       registered_at: DateTime.now)
+
     head :created
+  end
+
+  private
+
+  def device_params
+    params.require(:data)
+          .require(:attributes)
+          .permit(:token)
   end
 end
--- spec/requests/register_device_spec.rb
+++ spec/requests/register_device_spec.rb
@@ -15,10 +15,19 @@ RSpec.describe "POST /api/devices" do
   end
 
   context "with a valid request" do
+    let(:device) { Device.unscoped.last }
+
     it "returns a created status" do
       post "/api/devices", params: valid_parameters, headers: valid_authed_headers
 
       expect(response).to be_created
+    end
+
+    it "creates the device" do
+      expect { post "/api/devices", params: valid_parameters,
+                                    headers: valid_authed_headers }.to \
+        change { Device.count }.by 1
+      expect(device.token).to eq token
     end
   end
 end
