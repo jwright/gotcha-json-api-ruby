7e2a0511314a12a33588da45a9eb0a4de65eed00
Jamie Wright <jamie@brilliantfantastic.com>
2018-03-21 23:11:19 -0400

    Set the job queue adapter to test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
+  config.active_job.queue_adapter        = :test
+
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.perform_caching = false
