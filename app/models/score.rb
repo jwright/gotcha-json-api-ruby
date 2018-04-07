d9b36a994e5d86a9121deee3662dc081df4fcd71
Jamie Wright <jamie@brilliantfantastic.com>
2018-04-07 20:01:54 +0000

    Captured player creates a score with one point

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,8 @@ class Score < ApplicationRecord
   belongs_to :player
 
   validates :scored_at, presence: true
+
+  def self.captured_player!(arena, player)
+    self.create! arena: arena, player: player, scored_at: DateTime.now, points: 1
+  end
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,6 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Score do
+  describe ".captured_player!" do
+    let(:arena) { create :arena }
+    let(:player) { create :player, arenas: [arena] }
+
+    it "creates a new score with one point" do
+      score = described_class.captured_player! arena, player
+
+      expect(score).to be_valid
+      expect(score.scored_at).to be_within(1.second).of(DateTime.now)
+      expect(score.arena).to eq arena
+      expect(score.player).to eq player
+      expect(score.points).to eq 1
+    end
+  end
+
   describe "validations" do
     subject { build :score }
 
