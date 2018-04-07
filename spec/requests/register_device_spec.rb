9be46acfa4e2b5aaa87743711e33c7b175344430
Jamie Wright <jamie@brilliantfantastic.com>
2018-04-07 21:25:00 +0000

    Do not create a new device if one already exists for that token

--- spec/requests/register_device_spec.rb
+++ spec/requests/register_device_spec.rb
@@ -44,6 +44,16 @@ RSpec.describe "POST /api/devices" do
     end
   end
 
+  context "with a device already registered" do
+    let!(:device) { create :device, player: player, token: token }
+
+    it "returns an ok status" do
+      post "/api/devices", params: valid_parameters, headers: valid_authed_headers
+
+      expect(response).to be_ok
+    end
+  end
+
   context "with an incorrect json payload" do
     let(:parameters) do
       {
