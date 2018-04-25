e5b542e9e7eae48bea7db8bd6081f50899832855
Jamie Wright <jamie@brilliantfantastic.com>
2018-04-25 16:07:17 -0400

    Notifier to create a push notification to the opponent to trigger the code entry

---
+++ app/models/confirm_capture_notifier.rb
@@ -0,0 +1,29 @@
+class ConfirmCaptureNotifier
+  include PushNotificationNotifier
+
+  attr_reader :match
+
+  def initialize(match)
+    @match = match
+  end
+
+  def notify_player!(player)
+    notifications_for(player).each do |notification|
+      client.push notification unless notification.nil?
+    end
+  end
+
+  private
+
+  def notification(player, token)
+    if opponent = match.opponent_for(player)
+      notification = Houston::Notification.new(device: token)
+      notification.alert = "Gotcha! Looks like you met #{opponent.name}. "\
+                           "Please confirm with his code."
+      notification.category = :confirm_capture
+      notification.badge = Match.for(player).open.count
+      notification.custom_data = MatchSerializer.new(match).serializable_hash
+      notification
+    end
+  end
+end
