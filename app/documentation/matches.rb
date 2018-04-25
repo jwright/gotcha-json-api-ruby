eca156a79312dc93b9c73de388681554b98222a2
Jamie Wright <jamie@brilliantfantastic.com>
2018-04-25 17:01:58 -0400

    Add documentation for captured endpoint

--- app/documentation/matches.rb
+++ app/documentation/matches.rb
@@ -24,7 +24,7 @@ module Documentation
                   id: "12345",
                   type: "match",
                   attributes: {
-                    confirmation_code: null,
+                    confirmation_code: "",
                     found_at: 5632493294,
                     matched_at: 783287834764,
                   },
@@ -61,6 +61,114 @@ module Documentation
               end
               example name: JSONAPI::MEDIA_TYPE do
                 key :errors, "Unauthorized"
+              end
+            end
+          end
+        end
+
+        swagger_path "/matches/:id/captured" do
+          operation :post do
+            key :summary, "Mark the match as found between the seeker and the opponent"
+            key :description, "Marks the match as found if the confirmation "\
+                              "code matches. Scores the match and sends a "\
+                              "successful match push notification to the "\
+                              "seeker and the opponent."
+            key :tags, ["MATCHES"]
+            security do
+              key :Bearer, []
+            end
+            parameter do
+              key :name, :attributes
+              key :type, :object
+              key :in, :body
+              key :description, "The match information to confirm"
+              schema do
+                key :"$ref", :MatchInput
+              end
+            end
+            response 200 do
+              key :description, "A JSON API formatted representation of the single Match that was found."
+              schema do
+                key :"$ref", :Match
+              end
+              example name: JSONAPI::MEDIA_TYPE do
+                key :data, {
+                  id: "12345",
+                  type: "match",
+                  attributes: {
+                    confirmation_code: "0437",
+                    found_at: 5632493294,
+                    matched_at: 783287834764,
+                  },
+                  relationships: {
+                    arena: {
+                      data: {
+                        id: "67893",
+                        type: "arena"
+                      }
+                    },
+                    opponent: {
+                      data: {
+                        id: "53273",
+                        type: "player"
+                      }
+                    },
+                    seeker: {
+                      data: {
+                        id: "77349543",
+                        type: "player"
+                      }
+                    }
+                  }
+                }
+              end
+            end
+            response 400 do
+              key :description, "Confirmation code does not match"
+              schema do
+                key :type, :array
+                items do
+                  key :"$ref", :Error
+                end
+              end
+              example name: JSONAPI::MEDIA_TYPE do
+                key :errors, [{
+                  title: "Invalid parameter",
+                  detail: "Confirmation code does not match",
+                  status: 400
+                }]
+              end
+            end
+            response 401 do
+              key :description, "Unauthorized"
+              schema do
+                key :type, :array
+                items do
+                  key :"$ref", :Error
+                end
+              end
+              example name: JSONAPI::MEDIA_TYPE do
+                key :errors, [{
+                  title: "Not Authorized",
+                  detail: "Not Authorized",
+                  status: 401
+                }]
+              end
+            end
+            response 412 do
+              key :description, "Match passed in is not pending"
+              schema do
+                key :type, :array
+                items do
+                  key :"$ref", :Error
+                end
+              end
+              example name: JSONAPI::MEDIA_TYPE do
+                key :errors, [{
+                  title: "Precondition failed",
+                  detail: "Match was not pending",
+                  status: 412
+                }]
               end
             end
           end
@@ -149,7 +257,24 @@ module Documentation
                 end
               end
             end
+          end
+        end
 
+        swagger_schema :MatchInput do
+          key :type, :object
+          key :required, [:data]
+          property :data do
+            key :type, :object
+            key :required, [:attributes]
+            property :attributes do
+              key :type, :object
+              key :required, [:confirmation_code]
+              property :token do
+                key :type, :integer
+                key :format, :string
+                key :description, "The confirmation code used to confirm the capture"
+              end
+            end
           end
         end
 
