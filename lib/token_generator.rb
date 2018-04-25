287afbc96c7c4907be0d15af2345153286d1d44f
Jamie Wright <jamie@brilliantfantastic.com>
2018-04-25 08:12:24 -0400

    TokenGenerator can now generate codes

--- lib/token_generator.rb
+++ lib/token_generator.rb
@@ -4,4 +4,8 @@ class TokenGenerator
   def self.generate
     SecureRandom.base64(15).tr("+/=lIO0", "pqrsxyz")
   end
+
+  def self.generate_code(number_of_digits)
+    rand(10 ** number_of_digits).to_s
+  end
 end
--- spec/lib/token_generator_spec.rb
+++ spec/lib/token_generator_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe TokenGenerator do
       expect(tokens.uniq.length).to eq 50
     end
   end
+
+  describe ".generate_code" do
+    it "generates a unique numeric code" do
+      tokens = []
+      50.times do
+        tokens << described_class.generate_code(10)
+      end
+
+      expect(tokens.uniq.length).to eq 50
+    end
+  end
 end
