6022714d112ced72b8e32d9531d042a73f0bc341
Jamie Wright <jamie@brilliantfantastic.com>
2018-04-25 16:32:43 -0400

    Job to process a successful capture

--- app/jobs/successful_capture_job.rb
+++ app/jobs/successful_capture_job.rb
@@ -1,4 +1,16 @@
 class SuccessfulCaptureJob < ApplicationJob
+  queue_as :default
+
   def perform(match_id)
+    match = Match.find match_id
+
+    if match.found?
+      Score.captured_player!(match.arena, match.seeker)
+      Score.captured_player!(match.arena, match.opponent)
+      SuccessfulCaptureNotifier.new(match).notify!
+    end
+
+  rescue ActiveRecord::RecordNotFound => e
+    logger.error e
   end
 end
---
+++ app/models/successful_capture_notifier.rb
@@ -0,0 +1,7 @@
+class SuccessfulCaptureNotifier
+  def initialize(match)
+  end
+
+  def notify!
+  end
+end
